57ba6172b67de40e54cd883181e929dde61fce14
Suyash Srivastava <suyashsrivas02@gmail.com>
2020-07-20 09:33:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var router = require("./router")
+
+var mongo = require("./mongo")
+
+// Add ability to read json data from body
+app.use(
+    bodyParser.json({
+        limit: "50mb"
+    })
+);
+
+app.use("/api", router);
+
+
+module.exports = app;
